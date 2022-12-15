6b9f2e5c46328f610281a3960073af40655dabdf
Hugues Hoppe <hhoppe@gmail.com>
2022-12-14 16:35:46 -0800

    Minor.

--- advent_of_code_hhoppe/__init__.py
+++ advent_of_code_hhoppe/__init__.py
@@ -3,7 +3,7 @@
 
 from __future__ import annotations
 __docformat__ = 'google'
-__version__ = '0.8.0'
+__version__ = '0.8.1'
 __version_info__ = tuple(int(num) for num in __version__.split('.'))
 
 from collections.abc import Callable
