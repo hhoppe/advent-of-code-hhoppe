de3ccbf1599abed12c10b0b759e7f7f5658b79c0
Hugues Hoppe <hhoppe@gmail.com>
2023-01-05 23:23:02 -0800

    Create testdata.

--- test_advent_of_code_hhoppe.py
+++ test_advent_of_code_hhoppe.py
@@ -4,14 +4,14 @@
 
 import advent_of_code_hhoppe
 
-PROFILE = 'google.Hugues_Hoppe.965276'
-INPUT_URL = f'https://github.com/hhoppe/advent_of_code_{{year}}/raw/main/data/{PROFILE}/{{year}}_{{day:02d}}_input.txt'
-ANSWER_URL = f'https://github.com/hhoppe/advent_of_code_{{year}}/raw/main/data/{PROFILE}/{{year}}_{{day:02d}}{{part_letter}}_answer.txt'
+BASE_URL = ''https://github.com/hhoppe/advent-of-code-hhoppe/raw/main/testdata'
+INPUT_URL = f'{BASE_URL}/{{year}}_{{day:02d}}_input.txt'
+ANSWER_URL = f'{BASE_URL}/{{year}}_{{day:02d}}{{part_letter}}_answer.txt'
 
 def test_creation() -> None:
   """Test creation of Advent object."""
-  advent = advent_of_code_hhoppe.Advent(year=2020, input_url=INPUT_URL, answer_url=ANSWER_URL)
+  advent = advent_of_code_hhoppe.Advent(year=2017, input_url=INPUT_URL, answer_url=ANSWER_URL)
   puzzle = advent.puzzle(day=1)
-  assert len(puzzle.input) > 100
-  puzzle.verify(1, lambda x: 651651)
-  puzzle.verify(2, lambda x: 214486272)
+  assert len(puzzle.input) == 2119
+  puzzle.verify(1, lambda s: 1044)
+  puzzle.verify(2, lambda s: 1054)
